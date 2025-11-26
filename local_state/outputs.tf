e0a459d6650c7fb62b4c07b977d015089af19c36
Kosama1334 <75474064+Kosama1334@users.noreply.github.com>
2025-11-27 01:10:43 +0530

    Update outputs.tf - Add more output blocks

--- local_state/outputs.tf
+++ local_state/outputs.tf
@@ -1,6 +1,15 @@
-#first Output block
 
 output "instance_public_IP" {
   description = "get the pubic IP of the instance created"
   value = aws_instance.tf_first_instance.public_ip
+}
+
+output "instance_ipv6_address_count" {
+  description = "Get the number of IPv6 addresses"
+  value = aws_instance.tf_first_instance.ipv6_address_count
+}
+
+output "instance_private_IP" {
+  description = "get the private IP of the EC2 instance"
+  value = aws_instance.tf_first_instance.private_ip
 }
